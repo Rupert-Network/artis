1f92b476e3f19efb072c4c6bc90a89655cb53ea7
DeusFerrariis <cleghornw@protonmail.com>
2022-02-16 19:23:31 -0700

    snapshot test imger

--- imger_test.go
+++ imger_test.go
@@ -9,14 +9,20 @@ import (
 )
 
 func TestCannyEdge(t *testing.T) {
+	// Read in image
 	img, err := imgio.ImreadRGBA("images/iris01.jpeg")
 	assert.Nil(t, err)
 
+	// Create canny edges
 	edgeImg, err := edgedetection.CannyRGBA(img, 15, 45, 5)
 	assert.Nil(t, err)
 
-	err = imgio.Imwrite(edgeImg, "images/irisedge01.jpeg")
-	assert.Nil(t, err)
+	// Write image out to images for visual analysis
+	if _, err := imgio.ImreadRGBA("images/irisedge01.jpeg"); err != nil {
+		err = imgio.Imwrite(edgeImg, "images/irisedge01.jpeg")
+		assert.Nil(t, err)
+	}
 
+	// Compare canny edges with snapshot
 	cupaloy.SnapshotT(t, edgeImg)
 }
